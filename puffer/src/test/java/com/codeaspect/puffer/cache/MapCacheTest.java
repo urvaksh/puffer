b71f68af46e82f3dee233cee367106b8334d81b5
urvaksh <urvaksh.rogers@gmail.com>
2014-08-18 20:01:10 +0530

    Test Case

---
+++ puffer/src/test/java/com/codeaspect/puffer/cache/MapCacheTest.java
@@ -0,0 +1,33 @@
+package com.codeaspect.puffer.cache;
+
+import static junit.framework.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class MapCacheTest {
+	
+	private Cache<String,String> cache;
+	
+	@Before
+	public void setup(){
+		cache = new MapCache<String,String>();
+		cache.put("Key", "Value");
+	}
+
+	@Test
+	public void testContains(){
+		assertTrue(cache.contains("Key"));
+	}
+	
+	@Test
+	public void testGet(){
+		assertEquals("Value",cache.get("Key"));
+	}
+
+	@Test
+	public void testPut(){
+		cache.put("Key2", "Some Value");
+		assertTrue(cache.contains("Key2"));
+	}
+}
